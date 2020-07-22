d675633f8e0998fdf7455ff960197bf8a54f37f9
Bruno Lira <brunomarqueslira.one@gmail.com>
2020-07-22 09:12:27 -0300

    Mostrando dados e imagem do cliente para página de profile

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
     baseUrl: "http://localhost:8081",
-    bucketBaseUrl: "https://spring-boot-ionic-bml.s3-sa-east-1.amazonaws.com/"
+    bucketBaseUrl: "https://spring-boot-ionic-bml.s3-sa-east-1.amazonaws.com"
 }
